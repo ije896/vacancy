cf1b8b59d4b7421b4e32674c54f7fbe0cc15ec2d
iegan <ije@umail.ucsb.edu>
2018-02-27 12:46:16 -0800

    Initial start of JUCE project

---
+++ Source/PluginEditor.h
@@ -0,0 +1,36 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+
+//==============================================================================
+/**
+*/
+class VacancyAudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    VacancyAudioProcessorEditor (VacancyAudioProcessor&);
+    ~VacancyAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    VacancyAudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (VacancyAudioProcessorEditor)
+};
